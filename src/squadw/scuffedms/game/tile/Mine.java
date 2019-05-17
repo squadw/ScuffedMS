61695fbad3aad764624da10a351826b28d6f6b16
Austin Leuenhagen <fireball.al79@gmail.com>
2019-05-17 13:26:33 -0500

    remove duplicate code

--- src/squadw/scuffedms/game/tile/Mine.java
+++ src/squadw/scuffedms/game/tile/Mine.java
@@ -1,8 +1,6 @@
 package squadw.scuffedms.game.tile;
 
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class Mine extends Tile {
 
@@ -13,45 +11,13 @@ public class Mine extends Tile {
         exploded = false;
     }
 
+
     @Override
-    protected void mouseListener() {
-        getButton().addMouseListener(new MouseAdapter() {
-            boolean pressed;
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                pressed = true;
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                if (pressed) {
-                    if (getTileState() == MARKED && SwingUtilities.isRightMouseButton(e)) setClosed();
-                    else if (SwingUtilities.isRightMouseButton(e) && getTileState() != OPENED) setMarked();
-                    else explode();
-
-                    setImage();
-                    pressed = false;
-                }
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                pressed = false;
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                pressed = true;
-            }
-        });
-    }
-
-    public void explode() {
+    public void setOpened() {
+        super.setOpened();
         exploded = true;
-//        setImage(); - set image before ending the game so the bomb img still gets rendered and put on the board
-//        do something here to end the game probably just run method that ends game or switches to a new jframe with a "You lost! Play again?" screen
+        //setImage(); - set image before ending the game so the bomb img still gets rendered and put on the board
+        //do something here to end the game probably just run method that ends game or switches to a new jframe with a "You lost! Play again?" screen
     }
 
     @Override
